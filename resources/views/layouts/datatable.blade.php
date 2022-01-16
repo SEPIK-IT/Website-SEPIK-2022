5f8418e84b6600b8809f6a8f61e1ff290484e97f
Steven-Christando <c14200094@john.petra.ac.id>
2022-01-16 20:54:35 +0700

    pendaftaran

--- resources/views/layouts/datatable.blade.php
+++ resources/views/layouts/datatable.blade.php
@@ -7,7 +7,18 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    
+    <!-- JQuery -->
+    <!-- Jscript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+    <!-- Data Table -->
+    <!-- <link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
+    <script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script> -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script> -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
         @yield('judul')
     
   </head>
@@ -19,7 +30,7 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    @yield('datatable')
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
--- resources/views/pendaftaran.blade.php
+++ resources/views/pendaftaran.blade.php
@@ -2,23 +2,53 @@
 
 @section('judul')
   <title>Pendaftaran</title>
+
+  <style>
+      body {
+          background-color: blanchedalmond;
+          background-image: url('sepik-mlaku-mlaku.gif');
+          background-size: cover;
+      }
+      .style {
+        border-radius: 10px; 
+        background: rgba(255, 255, 255, 0.3)
+      }
+  </style>
 @endsection
 
 @section('isi')
-<table class="table table-hover" id = "example">
-        <thead class = "text-center">
-            <th>#</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Password</th>
-        </thead>
-        <tbody class = "text-center">
-            <tr>
-                <td>1</td>
-                <td>Steven</td>
-                <td>@gmail.com</td>
-                <td>12345678</td>
-            </tr>
-        </tbody>
-    </table>
+<div class="container">
+    <div class="row style mt-5">
+        <div class="col-12 p-3 table-responsive">
+            <table class="table table-hover" id = "example">
+                <thead class = "text-center">
+                    <th>#</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                </thead>
+                <tbody class = "text-center">
+                    <tr>
+                        <td>1</td>
+                        <td>Steven</td>
+                        <td>@gmail.com</td>
+                        <td>12345678</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('datatable')
+<script>
+$('#example').DataTable( {
+    searching: true,
+    show: true,
+    lengthChange: true,
+    paging: true,
+    ordering: true,
+} );
+</script>
 @endsection
