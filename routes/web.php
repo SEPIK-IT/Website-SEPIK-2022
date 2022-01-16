483cab1c0483f6383d86b875515b0198c0c2f0d7
Daniel Aprillio <danielaprillio02@gmail.com>
2022-01-16 16:07:27 +0700

    login register mechanic done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Auth::routes(['verify'=> true]);
