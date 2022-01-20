a7f24ea1f520eb1284847e9185cc4b7dc01b6e16
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-01-20 18:00:51 +0700

    init commit

---
+++ resources/views/sayembara.blade.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/sayembara-style.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <title>SEPIK 2022 - Sayembara</title>
+</head>
+<body>
+    <img class="background-image" src="{{ asset('img/bg.png') }}">
+    <div class="container">
+        <div class="title mtlarge">
+            <div class="title-header mx-xxl-5 px-xxl-5">
+                <div class="title-header mx-xxl-5 px-xxl-5">
+                    <div class="title-header mx-xxl-5 px-xxl-5">
+                        <div class="title-header mx-xxl-5 px-xxl-5 mt-5 pt-5">
+                            <h1 class="display-2 fw-bolder text-center">Sayembara Surabaya Epik</h1> 
+                        </div>
+                    </div>
+                </div> 
+            </div>
+            <div class="title-descriptions mx-xxl-5 px-xxl-5">
+                <div class="title-descriptions mx-xxl-5 px-xxl-5">
+                    <div class="title-descriptions mx-xxl-5 px-xxl-5 mt-4">
+                        <p class="fw-bold text-center">Tema dari Sayembara Epik 2022 adalah Nuwuhake Kreativitas saka Karya Epik. Akan terdapat 4 sayembara (lomba) guna menyemarakan SEPIK tahun ini.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mt-5 text-center">
+            <img src="{{ asset('img/semanggi.png') }}" style="width: 50px; height: 50px;">
+        </div>
+        <!--HUman Interest-->
+        <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+            <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+                <div class="mx-xxl-5 px-xxl-5">
+                    <table class="mx-auto">
+                        <tr>
+                            <td rowspan="5">
+                                <img src="{{ asset('img/kamera.png') }}" class="img-fluid mx-auto d-block">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h2>HUMAN INTEREST & STREET PHOTOGRAPHY</h2>
+                                <hr class="sayembaradivider">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p class="textjustify">Sayembara Human Interest & Street Photography ini akan dibuka untuk umum, baik untuk warga Surabaya maupun dari luar kota Surabaya. Sayembara ini dapat diikuti secara individu.</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="capsulebutton"><div class="innerbutton"></div></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">
+                                <div class="pendaftaranbutton"></div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!---->
+        <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+            <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+                <div class="mx-xxl-5 px-xxl-5">
+                    <table class="mx-auto">
+                        <tr>
+                            <td rowspan="5">
+                                <img src="{{ asset('img/kameravideo.png') }}" class="img-fluid mx-auto d-block">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h2 class="">SHORT CINEMATIC VIDEO COMPETITION</h2>
+                                <hr class="sayembaradivider">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p class="textjustify">Sayembara Cinematic Video ini dibuka untuk umum, baik untuk warga Surabaya maupun dari luar Kota Surabaya. Sayembara ini bersifat kelompok (tim) yang beranggotakan maksimal 3 orang per tim dan dapat diikuti secara individu.</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="capsulebutton"><div class="innerbutton"></div></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">
+                                <div class="pendaftaranbutton"></div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!---->
+        <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+            <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+                <div class="mx-xxl-5 px-xxl-5">
+                    <table class="mx-auto">
+                        <tr>
+                            <td rowspan="5">
+                                <img src="{{ asset('img/mashup.png') }}" class="img-fluid mx-auto d-block">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h2>MASH-UP LAGU</h2>
+                                <hr class="sayembaradivider">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p class="textjustify">Sayembara Mash-up lagu akan dibuka untuk umum, baik untuk warga Surabaya maupun dari luar Kota Surabaya. Lomba ini bersifat tim yang beranggotakan maksimal 3 anggota, yang sudah termasuk pemusik dan vokalis</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="capsulebutton"><div class="innerbutton"></div></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">
+                                <div class="pendaftaranbutton"></div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!---->
+        <div class="mx-xxl-5 px-xxl-5 mb-2 mt-5 pt-5">
+            <div class="mx-xxl-5 px-xxl-5 mt-5 pt-5">
+                <div class="mx-xxl-5 px-xxl-5">
+                    <table class="mx-auto">
+                        <tr>
+                            <td rowspan="5">
+                                <img src="{{ asset('img/cat.png') }}" class="img-fluid mx-auto d-block">
+                            </td>
+                        </tr>
+                        <tr class="ps-5">
+                            <td>
+                                <h2>DESAIN ILUSTRASI</h2>
+                                <hr class="sayembaradivider"">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Sayembara Desain Ilustrasi dibuka untuk umum, baik untuk warga Kota Surabaya maupun dari luar Kota Surabaya. Dengan tema yang diangkat yakni seputar Kebudayaan Surabaya, sayembara ini dapat diikuti secara individu.</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="capsulebutton"><div class="innerbutton"></div></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">
+                                <div class="pendaftaranbutton"></div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/sayembara', function () {
+    return view('sayembara');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
