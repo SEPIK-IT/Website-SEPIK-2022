3a7be69072ab14f9198255d1623a8bdb3363fe60
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-01-23 23:37:12 +0700

    splash_screen done

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,22 +1,23 @@
-<div id="mySidebar" class="sidebar">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <img style="width:150px;margin-left: 40px; margin-top: -30px;" src="{{ asset('img/iconwhite.png') }}">
+html =
+         <div id="mySidebar" class="sidebar">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <img style="width:150px;margin-left: 40px; margin-top: -30px;" src="{{ asset('img/iconwhite.png') }}">
+  <hr style="height:10px;border-width: 0px; background-color:
+#41464b; width: 80%; margin-left: 25px; margin-top: -7px;">
+  <a class="judul" href="/">Home</a>
+  <a class="judul" href="#">About</a>
+  <a class="judul" href="#papan">Timeline</a>
+  <a class="judul" href="#">Login</a>
+  <a class="judul" href="#signEvent">Events</a>
+  <a class="sub-judul" href="/sayembara">Sayembara</a>
+  <a class="sub-judul" href="#">Social Media Movement</a>
+  <a class="sub-judul" href="#">Donation</a>
+  <a class="sub-judul" href="#">Festival Budaya</a>
+  <a class="sub-judul" href="#">Closing Ceremony</a>
     <hr style="height:10px;border-width: 0px; background-color:
-  #41464b; width: 80%; margin-left: 25px; margin-top: -7px;">
-    <a class="judul" href="/">Home</a>
-    <a class="judul" href="#">About</a>
-    <a class="judul" href="#papan">Timeline</a>
-    <a class="judul" href="/login">Login</a>
-    <a class="judul" href="#signEvent">Events</a>
-    <a class="sub-judul" href="/sayembara">Sayembara</a>
-    <a class="sub-judul" href="#">socmed</a>
-    <a class="sub-judul" href="#">Social Media Movement</a>
-    <a class="sub-judul" href="#">Festival Budaya</a>
-    <a class="sub-judul" href="#">Closing Ceremony</a>
-      <hr style="height:10px;border-width: 0px; background-color:
-  #41464b; width: 80%; margin-left: 25px;  margin-top: -3px;">
-   <a class="judul" href="#">Contact</a>
-   <a class="sub-judul" href="#" style="font-size: 20px; padding-left: 32px;"><i class="fab fa-instagram"></i> @surabayaepik</a>
-  
-    <!-- <a href="#">Contact</a> -->
-  </div>
+#41464b; width: 80%; margin-left: 25px;  margin-top: -3px;">
+ <a class="judul" href="#">Contact</a>
+ <a class="sub-judul" href="#" style="font-size: 20px; padding-left: 32px;"><i class="fab fa-instagram"></i> @surabayaepik</a>
+
+  <!-- <a href="#">Contact</a> -->
+</div>
