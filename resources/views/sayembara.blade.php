9298b19d69c28b4d4e83d92288b7a4ad87c5f3a6
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-01-23 20:27:52 +0700

    pushing to sayembara

--- resources/views/sayembara.blade.php
+++ resources/views/sayembara.blade.php
@@ -6,11 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/sayembara-style.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <title>SEPIK 2022 - Sayembara</title>
 </head>
 <body class="m-0 p-0">
+    @extends('layouts.sidebar')
+    <button id="tooglebar" onclick="openNav()" style="background-color: transparent; z-index: 0; position: absolute; margin-left: 10%;"><i class="fas fa-bars fa-2x"></i></button>
     <img class="background-image" src="{{ asset('img/bg.png') }}">
     <div class="container">
         <div class="title mtlarge">
@@ -58,10 +61,10 @@
                         <tr>
                             <td>
                                 {{-- <div class="capsulebutton" style="cursor: pointer;" onclick="location.href='#'"><span class="position-relative text-end">test</span><div class="innerbutton text-center">test</div></div> --}}
-                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 25.000,-/individu</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white mx-auto" viewBox="0 0 16 16">
+                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 25.000,-/individu</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                                  </svg><div class="mx-auto text-white">SOP SAYEMBARA</div></div>
+                                  </svg><div class="mx-2 text-white">SOP SAYEMBARA</div></div>
                             </td>
                         </tr>
                         <tr>
@@ -96,10 +99,10 @@
                         </tr>
                         <tr>
                             <td>
-                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 30.000,-/tim</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white mx-auto" viewBox="0 0 16 16">
+                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 30.000,-/tim</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                                  </svg><div class="mx-auto text-white">SOP SAYEMBARA</div></div>
+                                  </svg><div class="mx-2 text-white">SOP SAYEMBARA</div></div>
                             </td>
                         </tr>
                         <tr>
@@ -134,10 +137,10 @@
                         </tr>
                         <tr>
                             <td>
-                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 30.000,-/tim</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white mx-auto" viewBox="0 0 16 16">
+                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 30.000,-/tim</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                                  </svg><div class="mx-auto text-white">SOP SAYEMBARA</div></div>
+                                  </svg><div class="mx-2 text-white">SOP SAYEMBARA</div></div>
                             </td>
                         </tr>
                         <tr>
@@ -172,10 +175,10 @@
                         </tr>
                         <tr>
                             <td>
-                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 25.000,-/individu</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white mx-auto" viewBox="0 0 16 16">
+                                <div style="cursor: pointer;" class="capsulebutton d-flex align-items-center justify-content-start"><div class="innerbutton d-flex align-items-center justify-content-center text-white">Rp 25.000,-/individu</div> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                                  </svg><div class="mx-auto text-white">SOP SAYEMBARA</div></div>
+                                  </svg><div class="mx-2 text-white">SOP SAYEMBARA</div></div>
                             </td>
                         </tr>
                         <tr>
@@ -188,5 +191,63 @@
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+        $(document).ready(function () {
+              var screenWidth = window.screen.width;
+
+              if (screenWidth<=400){
+                  $('#haha2').show();
+                  $('#haha').hide();
+              
+
+
+
+              
+                  
+              }
+              else{
+                  $('#haha').show();
+                  $('#haha2').hide();
+
+                  
+          
+                  
+                  
+              }
+              
+              
+
+});
+
+
+
+              var s = skrollr.init();
+      // $("body").fadeOut(1000, function(){redirectPage('home.html')});
+// 		$( "#banner" ).click(function() {
+//   $("body").fadeOut(1000);
+//   location.replace("https://www.w3schools.com");
+// });
+
+// $('.container-fluid').attr('data-1000','transform:translateX(-900%)');
+// alert($('.container-fluid').attr('data-1000'));
+$(document).on('click', "#banner", function(event) {
+  event.preventDefault();
+  linkLocation = 'www.youtube.com';
+  $("body").fadeOut(1000, function(){location.replace("login.html")});
+});
+
+
+function openNav() {
+document.getElementById("mySidebar").style.width = "250px";
+document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+document.getElementById("mySidebar").style.width = "0";
+document.getElementById("main").style.marginLeft = "0";
+}
+
+</script>
 </body>
 </html>
