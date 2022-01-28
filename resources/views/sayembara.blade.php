c452f1340be251f474beadfa98ca1c7142842f6b
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-01-28 17:58:36 +0700

    sayembara page changes

--- resources/views/sayembara.blade.php
+++ resources/views/sayembara.blade.php
@@ -69,7 +69,13 @@
                         </tr>
                         <tr>
                             <td class="pt-2">
+                                <div class="d-flex justify-content-evenly">
                                 <div class="pendaftaranbutton" style="cursor: pointer;" onclick="location.href='{{route('contestRegistration', ['competition' => 4])}}'"><p class="d-flex align-items-center justify-content-center" style="line-height: 50px; color: white;">PENDAFTARAN</p></div>
+                                <div class="lpobutton capsulebutton d-flex align-items-center justify-content-center" style="cursor: pointer; background: #686d5f; border-radius: 80px; height: 50px; width: 100%;" onclick="location.href='{{route('dllaporan')}}'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                  </svg><div class="ms-2 text-white" style="color: white;">LEMBAR ORISINALITAS</div></div>
+                                </div>
                             </td>
                         </tr>
                     </table>
@@ -107,7 +113,13 @@
                         </tr>
                         <tr>
                             <td class="pt-2">
+                                <div class="d-flex justify-content-evenly">
                                 <div class="pendaftaranbutton" style="cursor: pointer;" onclick="location.href='{{route('contestRegistration', ['competition' => 3])}}'"><p class="d-flex align-items-center justify-content-center" style="line-height: 50px; color: white;">PENDAFTARAN</p></div>
+                                <div class="lpobutton capsulebutton d-flex align-items-center justify-content-center" style="cursor: pointer; background: #686d5f; border-radius: 80px; height: 50px; width: 100%;" onclick="location.href='{{route('dllaporan')}}'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                  </svg><div class="ms-2 text-white" style="color: white;">LEMBAR ORISINALITAS</div></div>
+                                </div>
                             </td>
                         </tr>
                     </table>
@@ -145,7 +157,13 @@
                         </tr>
                         <tr>
                             <td class="pt-2">
+                                <div class="d-flex justify-content-evenly">
                                 <div class="pendaftaranbutton" style="cursor: pointer;" onclick="location.href='{{route('contestRegistration', ['competition' => 1])}}'"><p class="d-flex align-items-center justify-content-center" style="line-height: 50px; color: white;">PENDAFTARAN</p></div>
+                                <div class="lpobutton capsulebutton d-flex align-items-center justify-content-center" style="cursor: pointer; background: #686d5f; border-radius: 80px; height: 50px; width: 100%;" onclick="location.href='{{route('dllaporan')}}'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                  </svg><div class="ms-2 text-white" style="color: white;">LEMBAR ORISINALITAS</div></div>
+                                </div>
                             </td>
                         </tr>
                     </table>
@@ -185,7 +203,11 @@
                             <td class="pt-2">
                                 <div class="d-flex justify-content-evenly">
                                     <div class="pendaftaranbutton" style="cursor: pointer;" onclick="location.href='{{route('contestRegistration', ['competition' => 2])}}'"><p class="d-flex align-items-center justify-content-center" style="line-height: 50px; color: white;">PENDAFTARAN</p></div>
-                                    <div class="lpobutton" style="cursor: pointer; background: #686d5f; border-radius: 80px; height: 50px; width: 135%;" onclick="location.href='{{route('dllaporan')}}'"><p class="d-flex align-items-center justify-content-center text-center" style="line-height: 50px; color: white;">LEMBAR ORISINALITAS</p></div>
+                                    
+                                    <div class="lpobutton capsulebutton d-flex align-items-center justify-content-center" style="cursor: pointer; background: #686d5f; border-radius: 80px; height: 50px; width: 100%;" onclick="location.href='{{route('dllaporan')}}'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download text-white ms-2" viewBox="0 0 16 16">
+                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                      </svg><div class="ms-2 text-white" style="color: white;">LEMBAR ORISINALITAS</div></div>
                                 </div>
                             </td>
                         </tr>
