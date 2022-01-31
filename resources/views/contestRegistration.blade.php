b7c4ff16145ee23e9568a24a14c262585483b525
Rama Adi Nugraha <ramatabanan@gmail.com>
2022-02-01 02:23:33 +0800

    Change title

--- resources/views/contestRegistration.blade.php
+++ resources/views/contestRegistration.blade.php
@@ -12,7 +12,7 @@
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" href="/css/contestRegist.css">
-    <title>Contest Registration</title>
+    <title>{{$competition->title}}</title>
     @livewireStyles
 </head>
 
