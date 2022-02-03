7e10cf3b67db3e5ad947770b333505d09afd8157
Timothy Kristanto <66807730+TimothyKristanto@users.noreply.github.com>
2022-02-03 12:33:27 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ContestRegistController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DonationController;
 use App\Http\Controllers\DownloadController;
+use App\Http\Controllers\ZoopikRegistrationController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -31,6 +32,9 @@ Route::get('/sayembara', function () {
 });
 
 Route::get('/pameranVideo', 'App\Http\Controllers\linkController@index');
+
+Route::get('/zoopikRegistration', [ZoopikRegistrationController::class, 'index']);
+Route::post('/zoopikRegistration', [ZoopikRegistrationController::class, 'store']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
