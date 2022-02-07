86e415fd09d6ce63e76b358806a9069c423e82be
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-02-07 20:05:03 +0700

    asd

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,6 @@ Route::post('/donasi/donasi', [DonationController::class, 'store'])->middleware(
 
 Route::get('user-dashboard', [\App\Http\Controllers\UserDashboardController::class, 'index'])->middleware('auth')->name('user-dashboard');
 
-Route::view('social-media-movement', 'social-media-movement')->name('social-media-movement')->middleware('auth', 'isAdmin');
+Route::view('social-media-movement', 'social-media-movement')->name('social-media-movement')->middleware('auth');
 
 Auth::routes(['verify' => true]);
