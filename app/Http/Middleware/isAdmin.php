82939920820368d786a6f5f21b3f80aa97cd5628
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2022-02-07 19:36:03 +0700

    Update isAdmin.php

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -18,7 +18,7 @@ class isAdmin
     {   
         // $user = DB::table('users')->
         if(Auth::user()->is_admin != 1){
-            return redirect()->route('index');
+            return redirect()->route('homepage');
         }
         return $next($request);
     }
