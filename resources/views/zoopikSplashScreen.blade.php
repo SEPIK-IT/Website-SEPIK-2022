5e88992db3d340271cb47f5dfbd86b949c2f1648
Timothy Kristanto <66807730+TimothyKristanto@users.noreply.github.com>
2022-02-09 11:42:02 +0700

    Added links to LINE OC after registering zoopik

--- resources/views/zoopikSplashScreen.blade.php
+++ resources/views/zoopikSplashScreen.blade.php
@@ -12,7 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <script>
+    {{-- <script>
         var seconds=0;
         function timer(){
             seconds += 1;
@@ -23,24 +23,32 @@
             window.location.href='/home';
         }
         setTimeout(redirectPage, 2500);
-    </script>
+    </script> --}}
 </head>
 
 <body>
     <div class="container-fluid">
-        <div class="d-flex justify-content-center" style="height: 100vh;">
-                <div class="align-self-center text-center">
-                    <p class="fs-4">Anda telah mendaftar zoopik!</p>
-                    <p class="fs-4">Tunggu beberapa saat untuk kembali ke halaman utama</p>
-                </div>
-                <div class="dot">
-                    <figure></figure>
-                    <figure></figure>
-                    <figure></figure>
-                    <figure></figure>
-                    <figure></figure>
-                </div>
-
+        <div class="d-flex justify-content-center align-items-end" style="height: 60vh;">
+            <div class="text-center">
+                <p class="fs-4">Anda telah berhasil mendaftar zoopik!</p>
+                <p class="fs-4">Mohon bergabung dengan OpenChat LINE Zoopik pada link berikut:</p>
+                <ul>
+                    <li class="fs-4">OpenChat ZOOPIK 18 Februari 2022 : <a class="fs-5" href="https://bit.ly/OClineZoopik18Feb2022">https://bit.ly/OClineZoopik18Feb2022</a> </li>
+                    <li class="fs-4">OpenChat ZOOPIK 4 Maret 2022 : <a class="fs-5" href="https://bit.ly/OCLineZoopik4Maret2022">https://bit.ly/OCLineZoopik4Maret2022</a> </li>
+                </ul>
+                <p class="fs-4">Jika telah bergabung, tekan tombol dibawah untuk kembali ke halaman home</p>
+            </div>
+            
+            {{-- <div class="dot">
+                <figure></figure>
+                <figure></figure>
+                <figure></figure>
+                <figure></figure>
+                <figure></figure>
+            </div> --}}
+        </div>
+        <div class="text-center">
+            <a href="/home" class="btn btn-outline-dark fs-5">Home</a>
         </div>
     </div>
 
