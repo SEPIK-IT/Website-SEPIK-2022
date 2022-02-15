d8b5e0684b5bb4ac451b3b15566a42fe6df13e9d
Rama Adi Nugraha <ramatabanan@gmail.com>
2022-02-15 12:08:35 +0800

    fix done

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,6 @@ Route::view('social-media-movement', 'social-media-movement')->name('social-medi
 
 // Pengmas
 Route::get('social-media-movement/pengmas', [PengmasController::class, 'index'])->middleware('auth')->name('social-media-movement.pengmas.index');
-Route::post('social-media-movement/pengmas', [PengmasController::class, 'store'])->middleware('auth')->name('social-media-movement.pengmas.store');
 
 
 Auth::routes(['verify' => true]);
